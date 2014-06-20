4dc38a2eb257b0444d053cde9312c0533d508582
Andreas Moor <andreas.moor@cloudguard.ch>
2014-06-20 10:29:07 +0200

    upgraded libs

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "Send Atom and RSS feed updates via email"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.zip "0.1.1"]
-                 [com.draines/postal "1.10.2"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [ch.qos.logback/logback-classic "1.0.13"]]
+                 [com.draines/postal "1.11.1"]
+                 [org.clojure/tools.logging "0.3.0"]
+                 [ch.qos.logback/logback-classic "1.1.2"]]
   :main feedletter.core
   :uberjar-name "feedletter.jar")
