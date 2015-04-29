7b4584afb63a14b62116a413eaa259661a303a9b
Andreas Moor <andreas.moor@cloudguard.ch>
2015-04-29 08:42:27 +0200

    minor refactorings

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
                  [org.clojure/data.zip "0.1.1"]
                  [com.draines/postal "1.11.1"]
                  [org.clojure/tools.logging "0.3.0"]
-                 [ch.qos.logback/logback-classic "1.1.2"]]
+                 [ch.qos.logback/logback-classic "1.1.2"]
+                 [hiccup "1.0.5"]]
   :main feedletter.core
   :uberjar-name "feedletter.jar")
--- src/feedletter/core.clj
+++ src/feedletter/core.clj
@@ -6,35 +6,38 @@
             [clojure.edn :as edn]
             [clojure.data.zip.xml :as z]
             [clojure.tools.logging :as log]
-            [postal.core :as p])
+            [postal.core :as p]
+            [hiccup.core :as h])
   (:gen-class))
 
-(defn read-res [res]
-  (-> res
+(defn read-rsrc [rsrc]
+  (-> rsrc
       slurp
       edn/read-string))
 
-(def ^:dynamic *state-dir* (io/file ".state"))
+(def state-dir (io/file ".state"))
 
 (defn state-token [feed-name]
-  (io/file *state-dir* (str feed-name ".edn")))
+  (io/file state-dir (str feed-name ".edn")))
 
 (defn read-state [feed-name]
   (try
-    (read-res (state-token feed-name))
+    (read-rsrc (state-token feed-name))
     (catch Exception e #{})))
 
 (defn write-state! [feed-name s]
   (spit (state-token feed-name) (prn-str s)))
 
 (defn zip-feed [url]
-  (zip/xml-zip (xml/parse url)))
+  (try
+    (zip/xml-zip (xml/parse url))
+    (catch Exception e (log/error (.getMessage e)))))
 
 (defn escape-str [s]
   (s/escape s {\/ " "}))
 
-(defn parse-feed [feed-url]
-  (let [feed (zip-feed feed-url)]
+(defn parse-feed [url]
+  (let [feed (zip-feed url)]
     {:name (or (z/xml1-> feed :title z/text)           ;; Atom
                (z/xml1-> feed :channel :title z/text)  ;; RSS
                )
@@ -43,29 +46,36 @@
                  (seq (z/xml-> feed :channel :item))) ;; RSS
            ]
        {:title (z/xml1-> e :title z/text)
-        :link (or (z/xml1-> e :link (z/attr :href)) ;; Atom
-                  (z/xml1-> e :link z/text))}       ;; RSS
+        :link (or (z/xml1-> e :link (z/attr :href))   ;; Atom
+                  (z/xml1-> e :link z/text))          ;; RSS
+        :desc (or (z/xml1-> e :content z/text)        ;; Atom
+                  (z/xml1-> e :description z/text))}  ;; RSS
        )}))
 
 (defn process-feed [fd]
   (let [state (read-state (:name fd))
         fd' (update-in fd [:entries] (fn [ex] (remove #(some #{(escape-str (:title %))} state) ex)))]
-    (write-state! (escape-str (:name fd)) (reduce merge state (map #(escape-str (:title %)) (:entries fd'))))
-    fd'
-    ))
+    (write-state! (escape-str (:name fd))
+                  (reduce merge state (for [e (:entries fd')]
+                                        (escape-str (:title e)))))
+    fd'))
 
-(defn entry-str [entry]
-  (str (:title entry) "\n" (:link entry) "\n\n"))
+(defn msg-html [entries]
+  (h/html
+   (interpose [:div {:style "clear:both;"} [:hr]]
+              (for [entry entries]
+                [:div [:h2 [:a {:href (:link entry)} (:title entry)]] (if-let [desc (:desc entry)] [:p desc])]))))
 
 (defn make-msg [cfg fd]
-  (with-meta {:from (or (:from cfg) "feedletter")
+  (with-meta {:from (s/join [(:name fd) " <" (:from cfg) ">"])
               :to (:to cfg)
-              :subject (s/join " " [(or (:subject cfg) "Feed update:") (:name fd) "-" (count (:entries fd)) "new items"])
-              :body (apply str (map entry-str (:entries fd)))}
+              :subject (s/join [(:name fd) " feed: " (count (:entries fd)) " new items"])
+              :body [{:type "text/html; charset=utf-8"
+                      :content (msg-html (:entries fd))}]}
     (or (:smtp cfg) {})))
 
 (defn send-msg [msg]
-  (when (seq (:body msg))
+  (when (seq (:content (first (:body msg))))
     (log/debug "sending feedletter" (:subject msg))
     (p/send-message msg)))
 
@@ -73,11 +83,11 @@
   (log/error s))
 
 (defn -main [& args]
-  (let [cfg (read-res (first args))]
-    (doseq [f (:feeds cfg)]
-      (log/debug "processing" f)
-      (.mkdirs *state-dir*)
-      (->> f
+  (let [cfg (read-rsrc (first args))]
+    (doseq [url (:feeds cfg)]
+      (log/debug "processing" url)
+      (.mkdirs state-dir)
+      (->> url
            parse-feed
            process-feed
            (make-msg cfg)
