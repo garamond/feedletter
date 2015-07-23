a9fd8a40da8d7ee64ab42f3bd14976e08861a017
Andreas Moor <a@grmnd.ch>
2015-07-23 21:48:32 +0200

    updated deps

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject feedletter "0.2.0"
   :description "Send Atom and RSS feed updates via email"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.zip "0.1.1"]
-                 [com.draines/postal "1.11.1"]
-                 [org.clojure/tools.logging "0.3.0"]
-                 [ch.qos.logback/logback-classic "1.1.2"]
+                 [com.draines/postal "1.11.3"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [ch.qos.logback/logback-classic "1.1.3"]
                  [hiccup "1.0.5"]]
   :main feedletter.core
   :uberjar-name "feedletter.jar")
