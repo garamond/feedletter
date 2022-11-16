75f07e5a006908833517adc1486c9c3bcd005cfc
Andreas Moor <amoor@fastmail.net>
2022-11-16 21:19:53 +0100

    upgrade deps

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject feedletter "0.4.3-SNAPSHOT"
+(defproject feedletter "0.4.4-SNAPSHOT"
   :description "Send Atom and RSS feed updates via email"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojars.scsibug/feedparser-clj "0.4.0"]
-                 [com.draines/postal "1.11.4"]
-                 [com.taoensso/timbre "4.2.1"]
+                 [com.draines/postal "2.0.5"]
+                 [com.taoensso/timbre "6.0.1"]
                  [hiccup "1.0.5"]]
   :main feedletter.core
   :uberjar-name "feedletter.jar"
